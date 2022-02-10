105c07a3bcf0a7ea23f8e85100123ce68d73f879
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-09 17:36:46 -0800

    set baseline for main page

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-import { 
-    redirectIfLoggedIn, 
-    signInUser, 
-    signupUser,
-} from './fetch-utils.js';
+// import { 
+//     redirectIfLoggedIn, 
+//     signInUser, 
+//     signupUser,
+// } from './fetch-utils.js';
 
-const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
+// const signInForm = document.getElementById('sign-in');
+// const signInEmail = document.getElementById('sign-in-email');
+// const signInPassword = document.getElementById('sign-in-password');
 
-const signUpForm = document.getElementById('sign-up');
-const signUpEmail = document.getElementById('sign-up-email');
-const signUpPassword = document.getElementById('sign-up-password');
+// const signUpForm = document.getElementById('sign-up');
+// const signUpEmail = document.getElementById('sign-up-email');
+// const signUpPassword = document.getElementById('sign-up-password');
 
-// if user currently logged in, redirect
-redirectIfLoggedIn();
+// // if user currently logged in, redirect
+// redirectIfLoggedIn();
 
-signUpForm.addEventListener('submit', async(event)=>{
-    event.preventDefault();
-    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+// signUpForm.addEventListener('submit', async(event)=>{
+//     event.preventDefault();
+//     const user = await signupUser(signUpEmail.value, signUpPassword.value);
 
-    if (user){
-        redirectIfLoggedIn();
-    } else {
-        console.error(user);
-    }
-});
+//     if (user){
+//         redirectIfLoggedIn();
+//     } else {
+//         console.error(user);
+//     }
+// });
 
-signInForm.addEventListener('submit', async(event)=>{
-    event.preventDefault();
-    const user = await signInUser(signInEmail.value, signInPassword.value);
+// signInForm.addEventListener('submit', async(event)=>{
+//     event.preventDefault();
+//     const user = await signInUser(signInEmail.value, signInPassword.value);
   
-    if (user){
-        redirectIfLoggedIn();
-    } else {
-        console.error(user);
-    }
-});
+//     if (user){
+//         redirectIfLoggedIn();
+//     } else {
+//         console.error(user);
+//     }
+// });
