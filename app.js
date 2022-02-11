b4ef67f59f1ad6eb33676be94b35693849f92156
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-10 18:30:05 -0800

    started adding render function. adding classes. taking break

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const gameCardContainer = document.querySelector('.main-ul');
 
 async function fetchGames() {
     const games = await getGames();
-    console.log('hello', games);
+    // console.log('hello', games);
 
     for (let game of games) {
-        console.log('game', game);
+        // console.log('game', game);
         const gameEl = renderGameCard(game);
 
         gameCardContainer.append(gameEl);
--- render.utils.js
+++ render.utils.js
@@ -12,7 +12,7 @@ export function renderGameCard(games) {
     img.classList.add(`${games.game}-img`);
     a.href = `/games/?id=${games.id}`;
     div.classList.add('dropdown-img');
-console.log('render',games)
+    console.log('render', games);
     span.append(a);
     div.append(img);
     li.append(span, div);
@@ -20,6 +20,34 @@ console.log('render',games)
     return li;
 }
 
-export function renderFocusGame(){
-    
+export function renderFocusGame(game) {
+    const section = document.createElement('section');
+    const divContainer = document.createElement('div');
+    const divHeader = document.createElement('div');
+    const divMeta = document.createElement('div');
+    const divMetaRating = document.createElement('div');
+    const divMetaName = document.createElement('div');
+    const divIgn = document.createElement('div');
+    const divIgnRating = document.createElement('div');
+    const divIgnName = document.createElement('div');
+    const divIframe = document.createElement('div');
+    const iFrame = document.createElement('iframe');
+    const divDescription = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+
+    divContainer.classList.add('container');
+    divContainer.classList.add(`bg${game.id}-image2`);
+
+    divHeader.classList.add('game-header')
+    divHeader.classList.add(`bg${game.id}-image2`)
+
+
+    divMeta.append(divMetaName, divMetaRating);
+    divIgn.append(divIgnName, divIgnRating);
+    divIframe.append(iFrame);
+    divDescription.append(h2, p);
+    divContainer.append(divHeader, divMeta, divIgn, divIframe, divDescription);
+    section.append(divContainer);
+    return section;
 }
